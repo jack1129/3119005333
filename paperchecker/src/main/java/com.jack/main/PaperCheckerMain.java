5e0590c016f0bc3732cdf0b227864a79dd250939
jack1129 <1366460273@qq.com>
2021-09-15 22:29:47 +0800

    重命名

--- paperchecker/src/main/java/com.jack/main/PaperCheckerMain.java
+++ paperchecker/src/main/java/com.jack/main/PaperCheckerMain.java
@@ -2,11 +2,9 @@ package com.jack.main;
 
 import com.jack.utils.HammingUtils;
 import com.jack.utils.SimHashUtils;
-import com.jack.utils.TxtIOUtils;
+import com.jack.utils.TxtIO;
 
-import java.util.Scanner;
-
-public class MainPaperCheck {
+public class PaperCheckerMain {
 
     public static void main(String[] args) {
 //        开始未通过io流，直接控制台手动输入绝对路径来测试代码可行性
@@ -16,8 +14,8 @@ public class MainPaperCheck {
 //        args = s.split(" ");
 
         // 从命令行输入的路径名读取对应的文件，将文件的内容转化为对应的字符串
-        String str0 = TxtIOUtils.readTxt(args[0]);  // 读取原文的绝对路径
-        String str1 = TxtIOUtils.readTxt(args[1]);  // 读取抄袭版论文的绝对路径
+        String str0 = TxtIO.readTxt(args[0]);  // 读取原文的绝对路径
+        String str1 = TxtIO.readTxt(args[1]);  // 读取抄袭版论文的绝对路径
         String resultFileName = args[2];  // 输出答案文件的绝对路径
         // 由字符串得出对应的 simHash值
         String simHash0 = SimHashUtils.getSimHash(str0);
@@ -25,7 +23,7 @@ public class MainPaperCheck {
         // 由 simHash值求出相似度
         double similarity = HammingUtils.getSimilarity(simHash0, simHash1);
         // 把相似度写入最后的结果文件中
-        TxtIOUtils.writeTxt(similarity, resultFileName);
+        TxtIO.writeTxt(similarity, resultFileName);
         System.out.println("相似度录入文件成功！");
         // 退出程序
         System.exit(0);
---
+++ paperchecker/src/main/java/com.jack/utils/ShortTxtException.java
@@ -0,0 +1,20 @@
+package com.jack.utils;
+
+public class ShortTxtException extends Exception {
+    public ShortTxtException() {
+        super();
+    }
+
+    public ShortTxtException(String message) {
+        super(message);
+    }
+
+    public ShortTxtException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ShortTxtException(Throwable cause) {
+        super(cause);
+    }
+
+}
