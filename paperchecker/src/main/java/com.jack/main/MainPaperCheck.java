18886b2a5954b3aae3c218c833eb02e7c0ba153b
jack1129 <1366460273@qq.com>
2021-09-15 22:03:36 +0800

    将原来不在仓库中的项目重构，在此仓库提交

---
+++ paperchecker/src/main/java/com.jack/main/MainPaperCheck.java
@@ -0,0 +1,35 @@
+package com.jack.main;
+
+import com.jack.utils.HammingUtils;
+import com.jack.utils.SimHashUtils;
+import com.jack.utils.TxtIOUtils;
+
+import java.util.Scanner;
+
+public class MainPaperCheck {
+
+    public static void main(String[] args) {
+//        开始未通过io流，直接控制台手动输入绝对路径来测试代码可行性
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("请输入一个文件路径：");
+//        String s = scanner.nextLine();
+//        args = s.split(" ");
+
+        // 从命令行输入的路径名读取对应的文件，将文件的内容转化为对应的字符串
+        String str0 = TxtIOUtils.readTxt(args[0]);  // 读取原文的绝对路径
+        String str1 = TxtIOUtils.readTxt(args[1]);  // 读取抄袭版论文的绝对路径
+        String resultFileName = args[2];  // 输出答案文件的绝对路径
+        // 由字符串得出对应的 simHash值
+        String simHash0 = SimHashUtils.getSimHash(str0);
+        String simHash1 = SimHashUtils.getSimHash(str1);
+        // 由 simHash值求出相似度
+        double similarity = HammingUtils.getSimilarity(simHash0, simHash1);
+        // 把相似度写入最后的结果文件中
+        TxtIOUtils.writeTxt(similarity, resultFileName);
+        System.out.println("相似度录入文件成功！");
+        // 退出程序
+        System.exit(0);
+    }
+
+}
+
---
+++ paperchecker/src/main/java/com.jack/utils/SimHash.java
@@ -0,0 +1,4 @@
+package com.jack.utils;
+
+public class SimHash {
+}
